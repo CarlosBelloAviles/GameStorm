bebf57e067709fa5c481435ff40b3793fd9a20a5
Carlos_Esteban <carlos.belloaviles@gmail.com>
2025-11-05 17:05:06 -0300

    Update config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GameStorm/',
+  // No necesitamos base URL para Vercel
 })
