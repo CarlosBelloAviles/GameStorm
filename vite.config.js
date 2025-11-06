b7500118cc440591df6f7dd8a004220621b73000
Carlos_Esteban <carlos.belloaviles@gmail.com>
2025-11-06 00:51:53 -0300

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // No necesitamos base URL para Vercel
+  base: '/GameStorm/',
 })
